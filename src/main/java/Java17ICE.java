d3cfb2351f1b2e32cbd87054a5b4453ce3fd7f76
GriffinCodes <griffin@projecteden.gg>
2022-01-17 11:17:56 -0600

    Simplify

--- src/main/java/Java17ICE.java
+++ src/main/java/Java17ICE.java
@@ -2,15 +2,11 @@ import java.util.HashMap;
 
 public class Java17ICE {
 
-	interface Arg {}
-
-	record NumberBasedArg<T extends Number>(String name, T from, T to) implements Arg {}
-
 	public static void main(String[] args) {
-		final Arg abc = new NumberBasedArg<>("abc", 1, 2);
+		final Number abc = 1;
 		new HashMap<>() {{
-			if (abc instanceof NumberBasedArg numberBasedArg)
-				System.out.println(numberBasedArg.name);
+			if (abc instanceof Integer integer)
+				System.out.println(integer.intValue());
 		}};
 	}
 
